15b47523bcdd9b14090da55a44793b8086f051df
Andres Felipe Maldonado <andresmaldi@Andress-MacBook-Pro.local>
2023-04-10 17:09:38 -0500

    React book project

---
+++ src/components/BookList.js
@@ -0,0 +1,22 @@
+import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
+
+function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
+
+  const renderedBooks = books.map((book) => {
+    return (
+      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+    );
+  });
+  return (
+    <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>Click</button>
+      {renderedBooks}
+    </div>
+  );
+}
+
+export default BookList;
