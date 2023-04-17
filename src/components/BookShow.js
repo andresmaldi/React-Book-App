de47d43e7d37aa7398c260b2f7dda1906ad1d862
Andrés Felipe Maldonado <andresfmal@gmail.com>
2023-04-17 17:52:26 -0500

    Refactored application, added useBooksContext hook

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import BookEdit from "./BookEdit";
+import useBooksContext from "../hooks/use-books-context";
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
   const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useBooksContext();
 
   const handleDeleteClick = () => {
-    onDelete(book.id);
+    deleteBookById(book.id);
   };
 
-  const handleEditclick = () => {
+  const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, newTitle) => {
+  const handleSubmit = () => {
     setShowEdit(false);
-    onEdit(id, newTitle);
   };
 
   let content = <h3>{book.title}</h3>;
   if (showEdit) {
-    content = <BookEdit onSubmit={handleSubmit} onEdit={onEdit} book={book} />;
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
   }
 
   return (
@@ -27,7 +28,7 @@ function BookShow({ book, onDelete, onEdit }) {
       <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
       <div>{content}</div>
       <div className="actions">
-        <button className="edit" onClick={handleEditclick}>
+        <button className="edit" onClick={handleEditClick}>
           Edit
         </button>
         <button className="delete" onClick={handleDeleteClick}>
